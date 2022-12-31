5b58bbd1dd6c96a88e48d3c87f94ef0e356f998f
Egil TNC <egilmonsas@gmail.com>
2022-12-31 14:21:18 +0100

    wrote some tests which also caught a bug

--- src/erc/NSEN_1993.rs
+++ src/erc/NSEN_1993.rs
@@ -7,15 +7,14 @@ pub fn f_6_2(N_ed: f64, My_ed: f64, Mz_ed: f64, N_rd: f64, My_rd: f64, Mz_rd: f6
 
 /// Reduced capacity to account for shear forces
 /// Not necessary to reduce if Ved < 0.5 Vrd
-pub fn f_6_29(fy:f64, rho:f64)->f64{
-    (1.0-rho)*fy
+pub fn f_6_29(fy: f64, rho: f64) -> f64 {
+    (1.0 - rho) * fy
 }
 
 /// Reduction factor rho for shear when computing moment
-pub fn _compute_rho(Ved:f64,Vpl_rd:f64)->f64{
-    ((2.0*Ved)/(Vpl_rd)-1.0).powi(2)
+pub fn _compute_rho(Ved: f64, Vpl_rd: f64) -> f64 {
+    ((2.0 * Ved) / (Vpl_rd) - 1.0).powi(2)
 }
-
 
 /*----------------- BUCKLING ------------------*/
 
@@ -26,7 +25,10 @@ pub fn f_6_47(khi: f64, area: f64, fy: f64, gamma_1: f64) -> f64 {
 
 /// Buckling reduction factor
 pub fn f_6_49(phi: f64, lambda: f64) -> f64 {
-    1.0 / (phi + (phi.powi(2) - lambda.powi(2)))
+    // Calculate khi
+    let khi = 1.0 / (phi + (phi.powi(2) - lambda.powi(2)).sqrt());
+    // Upper bounded by 1.0
+    khi.clamp(0.0, 1.0)
 }
 
 pub fn _compute_phi(alpha: f64, lambda: f64) -> f64 {
@@ -34,9 +36,12 @@ pub fn _compute_phi(alpha: f64, lambda: f64) -> f64 {
 }
 
 pub fn _compute_lamba(area: f64, fy: f64, n_cr: f64) -> f64 {
-    (area * fy / n_cr).sqrt()
+    let lambda = (area * fy / n_cr).sqrt();
+    if lambda < 0.0 {
+        panic!("Lambda came out as a negative number, something has gone terribly wrong")
+    }
+    lambda
 }
-
 
 pub enum BuckleCurve {
     A0,
@@ -46,8 +51,8 @@ pub enum BuckleCurve {
     D,
 }
 
-impl BuckleCurve{
-    pub fn alpha(&self)->f64{
+impl BuckleCurve {
+    pub fn alpha(&self) -> f64 {
         match self {
             BuckleCurve::A0 => 0.13,
             BuckleCurve::A => 0.21,
@@ -68,10 +73,43 @@ mod tests {
         let util = f_6_2(100.0, 100.0, 100.0, 300.0, 300.0, 300.0);
         assert_zeq!(util, 1.0);
     }
+    #[test]
+    pub fn test_f_6_47() {
+        let khi = 0.6;
+        let area = 1000.0;
+        let fy = 355.0;
+        let gamma_1 = 1.15;
 
-    
+        let res = f_6_47(khi, area, fy, gamma_1);
+        assert_zeq!(res, 185217.391304);
+    }
+    #[test]
+    pub fn function_f_6_49_yields_correct_value_for_normal_input() {
+        assert_zeq!(f_6_49(2.2, 2.0), 0.320871);
+    }
+    #[test]
+    pub fn function_f_6_49_clamps_result_correctly() {
+        assert_zeq!(f_6_49(0.2, 0.0), 1.0);
+    }
+
     #[test]
     fn test_buckle_curve_function() {
         assert_zeq!(BuckleCurve::A0.alpha(), 0.13);
+    }
+
+    #[test]
+    fn test_lambda_function() {
+        let a = 1000.0;
+        let fy = 355.0;
+        let n_cr = 10000.0;
+        let res = _compute_lamba(a, fy, n_cr);
+        assert_zeq!(res, 5.958187);
+    }
+    #[test]
+    fn test_phi_function() {
+        let alpha = 0.2;
+        let lambda = 2.0;
+        let res = _compute_phi(alpha, lambda);
+        assert_zeq!(res, 2.68);
     }
 }
